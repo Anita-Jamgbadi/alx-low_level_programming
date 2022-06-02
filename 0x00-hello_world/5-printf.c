44245708c9ce9be426e01818c41c53478dcfa75e
anita jamgbadi <jamgbanita@gmail.com>
2022-06-02 22:40:22 +0000

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - required to begin the program
+ * Returns: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

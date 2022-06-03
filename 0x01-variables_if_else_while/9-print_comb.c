4ca453554bddc985021fbdbcf8fb00e9e9dd63d6
anita jamgbadi <jamgbanita@gmail.com>
2022-06-03 23:02:10 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations
+ * of single digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		putchar(n);
+
+		if (n <= 8)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			break;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+

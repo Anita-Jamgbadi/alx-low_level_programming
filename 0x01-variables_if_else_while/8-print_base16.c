bcf4dbe7cbec44597bb313247c65803d5a14ce94
anita jamgbadi <jamgbanita@gmail.com>
2022-06-03 22:46:10 +0000

    count in hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - count base 16
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	char c;
+
+	n = 0;
+	c = 'a';
+
+	while (n <= 16)
+	{
+		if (n < 10)
+		{
+			putchar(n % 10 + '0');
+		}
+		else
+		{
+			putchar(c);
+			c++;
+		}
+		n++;
+	}
+	return (0);
+}

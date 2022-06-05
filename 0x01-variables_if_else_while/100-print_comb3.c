e6c151e456c7b2a26a03583446b9c80d7d8a5f1a
anita jamgbadi <jamgbanita@gmail.com>
2022-06-05 12:39:09 +0000

    Voluntary tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -print all possible combos of 2 digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n, a;
+	
+	for (n = 0; n < 10; n++)
+	{
+		for (a = 0; a < 10; a++)
+		{
+			u = na;
+

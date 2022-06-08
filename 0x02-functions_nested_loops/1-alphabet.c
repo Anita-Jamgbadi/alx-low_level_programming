423f78e5833bad55bb3a57aa6e1ce397900e536b
anita jamgbadi <jamgbanita@gmail.com>
2022-06-08 20:02:33 +0000

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ * and a new line
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char currentLetter;
+	currentLetter = 'a';
+
+	while (currentLetter <= 'z')
+	{
+		_putchar(currentLetter);
+		currentLetter++;
+	}
+	_putchar('\n');
+	return (0);
+}

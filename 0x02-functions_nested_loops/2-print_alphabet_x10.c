5199714a727f03c6de8fb41c332c4f8d7ee4ded7
anita jamgbadi <jamgbanita@gmail.com>
2022-06-08 23:07:43 +0000

    Task 2 + task 2 fixs

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,14 +11,16 @@ void print_alphabet_x10(void)
 	int i;
 
 	ch = 'a';
+	i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (i < 10)
 	{
 		while (ch <= 'z')
 		{
 			_putchar(ch);
 			ch++;
 		}
+		i++;
 		_putchar('\n');
 	}
 }

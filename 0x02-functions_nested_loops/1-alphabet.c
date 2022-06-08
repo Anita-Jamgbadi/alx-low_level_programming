ad04068e02312a5edf3a541ae31226a9abf7334b
anita jamgbadi <jamgbanita@gmail.com>
2022-06-08 20:18:51 +0000

    Task 2 + task 2 fixs

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet in lowercase
+ * print_alphabet - Prints the alphabet in lowercase
  * and a new line
  * Return: 0 always
  */
@@ -9,6 +9,7 @@
 void print_alphabet(void)
 {
 	char currentLetter;
+
 	currentLetter = 'a';
 
 	while (currentLetter <= 'z')

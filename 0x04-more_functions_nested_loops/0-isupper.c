52205dca3d2eabd0abc075299fa40808d74d7175
anita jamgbadi <jamgbanita@gmail.com>
2022-06-09 23:32:00 +0000

    0X04

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is uppercase
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	int value;
+
+	if (c <= 'A' && c >= 'Z')
+	{
+		value = 1;
+	}
+	else
+	{
+		value = 0;
+	}
+	return (value)
+}

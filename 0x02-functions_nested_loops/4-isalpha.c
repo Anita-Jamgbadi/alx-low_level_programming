d1dd1eca90f532fe9b88e2d764b1f6e24892c7cd
anita jamgbadi <jamgbanita@gmail.com>
2022-06-12 14:45:42 +0000

    Redone task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: character to be scrutinized
- * Return: 1 if true, 0 if false
+ * Return: 1 if true and 0 if false
  */
 
 int _isalpha(int c)

a1a8021a88be367825444402e904609d19d01c39
anita jamgbadi <jamgbanita@gmail.com>
2022-06-14 21:32:26 +0000

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,9 +12,15 @@ int _atoi(char *s)
 
 	for(i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] >= 0 && s[i] <= 9) || (s[i] == '+' || s[i] == '-'))
+		if (s[i] == '+' || s[i] == '-')
+		{
+			res = s[i++];
+			break;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			res = s[i];
+			break;
 		}
 		else
 		{

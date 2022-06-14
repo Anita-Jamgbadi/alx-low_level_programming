a0342f367b616fb0b95a50892b8a01a7c9400e60
anita jamgbadi <jamgbanita@gmail.com>
2022-06-14 11:59:18 +0000

    reverse but dont print

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: points to string to be reversed
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	*s = s[len];
+}

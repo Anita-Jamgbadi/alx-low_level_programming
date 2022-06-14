c19d76394fddd8620a3fcdd2da6526c87bf58b9a
anita jamgbadi <jamgbanita@gmail.com>
2022-06-14 20:47:56 +0000

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,15 +14,15 @@ void print_array(int *a, int n)
 	i = 0;
 	while (a[i] != '\0' && i <= n)
 	{
-		if (i == n)
-		{
-			printf("%d", a[i]);
-			break;
-		}
-		else
+		if (i != n)
 		{
 			printf("%d, ", a[i]);
 		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+
 		i++;
 	}
 }

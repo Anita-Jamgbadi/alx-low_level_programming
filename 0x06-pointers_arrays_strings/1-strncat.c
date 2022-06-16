594938f7ee3cc83534ea0204fe79dbb87023a9e9
anita jamgbadi <jamgbanita@gmail.com>
2022-06-16 09:21:20 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - Strncat concats n characters from @src to @dest
+ * @src: src
+ * @dest: dest
+ * @n: int
+ * Return: @dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, destCount, k;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		destCount++;
+	}
+
+	dest[destCount] = ' ';
+
+	for (k = 0; k <= n; k++)
+	{
+		dest[destCount++] = src[k];
+	}
+
+	dest[destCount] = '\0';
+	return (dest);
+}

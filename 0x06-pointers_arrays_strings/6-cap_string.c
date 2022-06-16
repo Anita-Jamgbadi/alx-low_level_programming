d49c740cd7a1cb91a15c969b12c5b0137965e2e9
anita jamgbadi <jamgbanita@gmail.com>
2022-06-16 11:01:10 +0000

    all caps

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - to uppercase
+ * @c: string
+ * Return: @c
+ */
+
+char *cap_string(char *c)
+{
+	int i;
+
+	i = 0;
+	while (c[i] != '\0')
+	{
+		if ((c[i] >= 'A' || c[i] <= 'Z') || (c[i] >= 'a' || c[i] <= 'z'))
+		{
+			if (c[i] >= 'A' || c[i] <= 'Z')
+			{
+				c[i] = c[i];
+			}
+			else
+			{
+				c[i] = c[i] - 32;
+			}
+		}
+		i++;
+	}
+	return (c);
+}

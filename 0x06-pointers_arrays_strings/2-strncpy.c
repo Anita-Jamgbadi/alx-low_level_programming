fa5eff740a67884a80eb5c4a8da2baa714164aa6
anita jamgbadi <jamgbanita@gmail.com>
2022-06-16 09:51:59 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n bytes of src to dest
+ * @src: src
+ * @dest: dest
+ * @n: no of bytes
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, k;
+
+	k = 0;
+
+	if (n == 0)
+	{
+		dest[k] = '\0';
+	}
+	else
+	{
+		for (i = 0; src[i] != '\0' && i < n; i++)
+		{
+			dest[i] = src[i];
+		}
+	}
+
+	return (dest);
+}

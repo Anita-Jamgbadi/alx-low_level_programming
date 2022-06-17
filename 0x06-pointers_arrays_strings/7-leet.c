49574838bc8b26f008d3928650452fd8508e0b9d
anita jamgbadi <jamgbanita@gmail.com>
2022-06-17 21:52:35 +0000

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,45 +10,26 @@ char *leet(char *c)
 {
 	int i;
 
+	i = 0;
 	while (c[i] != '\0')
 	{
-		if (c[i] == 'a')
+		if (c[i] == 'a' || c[i] == 'A')
 		{
 			c[i] = '4';
 		}
-		else if (c[i] == 'A')
-		{
-			c[i] = '4';
-		}
-		else if (c[i] == 'e')
+		else if (c[i] == 'e' || c[i] == 'E')
 		{
 			c[i] = '3';
 		}
-		else if (c[i] == 'E')
-		{
-			c[i] = '3';
-		}
-		else if (c[i] == 'o')
+		else if (c[i] == 'o' || c[i] == 'O')
 		{
 			c[i] = '0';
 		}
-		else if (c[i] == 'O')
-		{
-			c[i] = '0';
-		}
-		else if (c[i] == 't')
+		else if (c[i] == 't' || c[i] == 'T')
 		{
 			c[i] = '7';
 		}
-		else if (c[i] == 'T')
-		{
-			c[i] = '7';
-		}
-		else if (c[i] == 'l')
-		{
-			c[i] = '1';
-		}
-		else if (c[i] == 'L')
+		else if (c[i] == 'l' || c[i] == 'L')
 		{
 			c[i] = '1';
 		}

fe27123ad3b43f2e3164021e302e9228a125e6c9
anita jamgbadi <jamgbanita@gmail.com>
2022-06-17 20:36:24 +0000

    to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,9 +12,13 @@ char *string_toupper(char *c)
 
 	while (c[i] != '\0')
 	{
-		if (c[i] >= 'a' || c[i] <= 'z')
+		if (c[i] >= 'a' && c[i] <= 'z')
 		{
 			c[i] = c[i] - 32;
+		}
+		else
+		{
+			c[i] = c[i];
 		}
 		i++;
 	}

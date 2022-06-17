715fd4008b06e57b6dc538fdb76c75dedfb6c902
anita jamgbadi <jamgbanita@gmail.com>
2022-06-17 19:57:19 +0000

    strcpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,15 +8,18 @@
  * Return: dest
  */
 
-char * _strncpy(char *dest, const char *src, int n)
+char *_strncpy(char *dest, const char *src, int n)
 {
 	int i;
 	
 	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		 dest[i] = src[i];
+	}
 	for ( ; i < n; i++)
+	{
 		dest[i] = '\0';
-	
+	}
 	return dest;
 			    
 }

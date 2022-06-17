ff455e76d588608aabe43bce5d5cc53191e35597
anita jamgbadi <jamgbanita@gmail.com>
2022-06-17 22:31:22 +0000

    print int

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_number - prints int;
+ * @n: int to be printed
+ */
+
+void print_number(int n)
+{
+	if (n < 10)
+	{
+		_putchar(n + '0');
+	}
+	if (n > 9 && n < 100)
+	{
+		_putchar((n % 10) + '0');
+		_putchar((n / 10) + '0');
+	}
+}

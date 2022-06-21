f90324f7496a199a11dcbdc7a5d6d406c5a045c4
anita jamgbadi <jamgbanita@gmail.com>
2022-06-21 21:51:29 +0000

    strchgr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,22 +12,20 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	i = 0;
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == c && c != '\0')
+		if (s[i] == c)
 		{
 			return (&s[i]);
-		}
-		if (c == '\0' && s[i++] == '\0')
-		{
-			return (&s[i++]);
 		}
 		else
 		{
 			continue;
 		}
-		i++;
+	}
+	if (c == '\0' && s[i] == '\0')
+	{
+		return (&s[i]);
 	}
 	return (NULL);
 }

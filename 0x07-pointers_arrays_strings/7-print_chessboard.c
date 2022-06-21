9ec7f69b3677093c74b4ddee51d6ed71ba2954cc
anita jamgbadi <jamgbanita@gmail.com>
2022-06-21 12:00:49 +0000

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard
+ * @a: pointer
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < 8)
+	{
+		while (*a[j] != '\0')
+		{
+			_putchar(*a[j]);
+			j++;
+		}
+		i++;
+	}
+}

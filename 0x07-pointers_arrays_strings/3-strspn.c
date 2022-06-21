6518001cfd35c54ca4ff3876ba2d49b5550df981
anita jamgbadi <jamgbanita@gmail.com>
2022-06-21 09:50:49 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - find how many bytes of *s only contain
+ * bytes fromaccept
+ * @s: library
+ * @accept: book
+ * Return: n
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n, i, res;
+
+	n = 0;
+	i = 0;
+	while (s[n] != '\0')
+	{
+		while (accept[i] != '\0')
+		{
+			if (accept[i] == s[n])
+				res = n;
+			i++;
+		}
+		n++;
+	}
+	return (res);
+}

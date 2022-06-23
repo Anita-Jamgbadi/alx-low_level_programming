787bb3371f1d55ffac56ac8407780bca4e59e2bf
anita jamgbadi <jamgbanita@gmail.com>
2022-06-23 12:26:06 +0000

    Prime number checker

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Checks for prime number
+ * @n: int
+ */
+
+int is_prime_number(int n)
+{
+	int i = 2;
+	
+	if (i == 1)
+	{
+		return (1);
+	}
+	return (isPrime (i, n));
+}
+
+/**
+ * isPrime - evaluate if number is prime
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int isPrime(int a, int b)
+{
+	if (b < 2 || b % a == 0)
+		return (0);
+	else if (a > b / 2)
+		return (1);
+	else
+		return (isPrime(a + 1, b));
+}

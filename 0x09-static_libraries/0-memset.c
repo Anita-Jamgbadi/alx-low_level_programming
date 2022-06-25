b9fcfa777c5524d3ef153c55b4a3c53012f50904
anita jamgbadi <jamgbanita@gmail.com>
2022-06-25 04:06:05 +0000

    create library

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memset - set memory toa constant byte
+ * @b: byte
+ * @s: dest
+ * @n: memory count
+ * Return: s/
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	int j = 0;

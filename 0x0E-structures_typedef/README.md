450cd6e6447aa66b7f468cc83e0753086d8ce7cc
anita jamgbadi <jamgbanita@gmail.com>
2022-07-05 20:02:53 +0000

    Create dog structure

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+First Venture into Structures in C
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - contains members of dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} doggie;
+#endif

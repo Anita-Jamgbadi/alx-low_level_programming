a72942e363a14e9f02e04d44aa02b071b00daeb2
anita jamgbadi <jamgbanita@gmail.com>
2022-07-09 00:28:39 +0000

    Printing strings using a variadic function

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - invoker
@@ -8,5 +10,10 @@
 int main(void)
 {
 	print_numbers(", ", 4, 0, 98, -1024, 402);
+	print_numbers("; ", 5, 30, 938, -123024, 43402, 0);
+	print_numbers("; ", 1, 30);
+	print_numbers("", 4, 304, 76, 87236, 28376);
+	print_numbers(NULL, 4, 304, 76, 87236, 28376);
+	print_numbers("; ", 0);
 	return (0);
 }
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - Print strings using variadic function
+ * @separator: separates each string
+ * @n: number of strings
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+	char *s;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(ap, char *);
+		
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%c", *separator);
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}

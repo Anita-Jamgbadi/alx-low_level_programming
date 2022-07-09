793a195e21dc1d41cd1abccd2b2368c7a6312c30
anita jamgbadi <jamgbanita@gmail.com>
2022-07-09 00:39:23 +0000

    Printing strings using a variadic function

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - check code
@@ -8,5 +10,11 @@
 int main(void)
 {
 	print_strings(", ", 2, "Jay", "Django");
+	print_strings("; ", 4, "You", "know", "nothing", "Jon Snow");
+	print_strings("; ", 1, "You know nothing, Jon Snow");
+	print_strings("", 4, "You ", "know ", "nothing, ", "Jon Snow");
+	print_strings(NULL, 4, "You ", "know ", "nothing, ", "Jon Snow");
+	print_strings("; ", 0);
+	print_strings("; ", 4, "You know", "", "nothing", NULL);
 	return (0);
 }

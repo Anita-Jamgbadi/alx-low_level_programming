2f3835ee25b6d16e38cbb5b61c235b8410166ee2
anita jamgbadi <jamgbanita@gmail.com>
2022-07-09 00:41:16 +0000

    Printing strings using a variadic function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (separator != NULL && i != n - 1)
 		{
-			printf("%c ", *separator);
+			printf("%s", separator);
 		}
 	}
 	printf("\n");

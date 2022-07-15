ad3e7275ce8078f8b72f65c7836e7e65b841e31a
anita jamgbadi <jamgbanita@gmail.com>
2022-07-15 11:12:20 +0000

    Print list elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@
  */
 
 size_t print_list(const list_t *h)
-{ 
+{
 	int elements;
 
 	if (h == NULL)
@@ -28,7 +28,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s\n",h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 			elements++;
 		}
 		h = h->next;

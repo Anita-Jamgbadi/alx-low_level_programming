e83bf9b553f9c6ed0ee5ef595d15ff8c85feee4f
anita jamgbadi <jamgbanita@gmail.com>
2022-07-16 05:46:27 +0000

    Print before main is executed

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -6,9 +6,11 @@
  **
  ** Return: Always 0.
  **/
+
+void print_first(void);
 int main(void)
 {
-	print_first;
+	print_first();
 	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 	return (0);
 }

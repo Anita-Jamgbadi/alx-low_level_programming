47e7468015b597667023c5d4b0e7929b61d9a3ef
anita jamgbadi <jamgbanita@gmail.com>
2022-07-19 12:23:30 +0000

    Sum node data from a list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_listint - sum all data in the nodes of a given list
+ * @head: head node of list
+ */
+
+int sum_listint(listint_t *head)
+{
+	int nSum;
+	listint_t *temp;
+
+	if (head == NULL)
+		return (0);
+
+	temp = head;
+	nSum = 0;
+	while (temp != NULL)
+	{
+		nSum += temp->n;
+		temp = temp->next;
+	}
+	return (nSum);
+}

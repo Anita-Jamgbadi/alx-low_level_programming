4cd0c1f78a1ef74543e9b22bd988e0b9fd197dd1
anita jamgbadi <jamgbanita@gmail.com>
2022-07-19 12:16:04 +0000

    fix get node at nth position

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,18 +13,27 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int nodeCount;
+	listint_t *temp;
 	listint_t *nthNode;
 
 	if (head == NULL)
 		return (NULL);
 
 	nodeCount = 0;
-	while (nodeCount < index && head->next != NULL)
+	temp = head;
+	while (temp != NULL)
 	{
-		if (head->next != NULL)
-			head = head->next;
-		else
-			return (NULL);
+		temp = temp->next;
+		nodeCount++;
+	}
+	if (index > nodeCount)
+		return (NULL);
+
+	nodeCount = 0;
+	while (nodeCount < index)
+	{
+		head = head->next;
+		nodeCount++;
 	}
 	nthNode = head;
 

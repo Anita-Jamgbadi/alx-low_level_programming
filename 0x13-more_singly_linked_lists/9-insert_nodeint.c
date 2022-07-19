23626f62a85e343eb3a74694644024736df92571
anita jamgbadi <jamgbanita@gmail.com>
2022-07-19 12:52:21 +0000

    insert node at nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *newNode, *temp, *temp2;
-	unsigned int nodeCount;
+	unsigned int nodeCount, zero;
 
 	if (*head == NULL)
 		return (NULL);
@@ -24,18 +24,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	temp = *head;
 
-	nodeCount = 0;
+	nodeCount = zero = 0;
 	while (nodeCount < idx)
 	{
 		temp = temp->next;
 		nodeCount++;
 	}
-	if (idx > nodeCount)
+	if (idx > nodeCount || idx < zero)
 		return (NULL);
 
 	temp2 = *head;
 	nodeCount = 0;
-	while (nodeCount < idx-1)
+	while (nodeCount < (idx - 1))
 	{
 		temp2 = temp2->next;
 		nodeCount++;

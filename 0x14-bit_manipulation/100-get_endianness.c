b9cccdc8136bbc18cfbff5c94e5ec8f2a8c45ba2
anita jamgbadi <jamgbanita@gmail.com>
2022-07-22 15:39:10 +0000

    get endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_endianness - check endianness
+ * Return: 0 or 1
+ */
+
+int get_endianness(void)
+{
+	unsigned int i;
+	char *c;
+
+	i = 1;
+	*c = (char *) & i;
+
+	if (*c)
+		return (1);
+	else
+		return (0);
+}
+

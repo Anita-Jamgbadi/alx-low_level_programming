319b44b65d1e94c7f4edd132a8ea8b823de721ac
anita jamgbadi <jamgbanita@gmail.com>
2022-07-25 12:52:41 +0000

    Read from an exizting file and print to screen

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+/**
+ * read_textfile - read the text content of a given file
+ * @filename: filename(FD)
+ * @letters: number of letters to be read from the file
+ * Return: actual number of letters read or 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	unsigned int fd, i, count;
+	unsigned int rd_success;
+	char *cell;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	i = -1;
+	if (fd == i)
+		return (0);
+
+	cell = malloc(sizeof(char *) * letters);
+	if (cell == NULL)
+		return (0);
+
+	read(fd, cell, letters);
+
+	rd_success = count = 0;
+	while (count < letters)
+	{
+		printf("%c", filename[count]);
+		count++;
+		rd_success++;
+	}
+	if (rd_success < letters)
+		return (0);
+	close(fd);
+	return (rd_success);
+}

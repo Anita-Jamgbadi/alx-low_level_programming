ac44f8d72e4db41dc3941dcd8134914135a6ead2
anita jamgbadi <jamgbanita@gmail.com>
2022-07-25 22:56:05 +0000

    create and write to a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,15 +21,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
-	if (access(filename, F_OK) == 0)
-	{
-		fd = open(filename, O_WRONLY);
-	}
-	else
-	{
-		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR, 0600);
-	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)

fd6e72ef382ab316d1a03128ad61d85232254148
anita jamgbadi <jamgbanita@gmail.com>
2022-07-25 22:54:12 +0000

    create and write to a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		fd = open(filename, O_CREAT | O_WRONLY, 0600);
+		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR, 0600);
 	}
 	if (fd == -1)
 		return (-1);

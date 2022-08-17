ebfc550d9ba8d39e6490cdfd8a30b7713c8bb8a0
anita jamgbadi <jamgbanita@gmail.com>
2022-08-17 10:01:08 +0000

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,7 +11,7 @@ void print_line(int n)
 	int i = 0;
 
 	if (n == 0)
-		_putchar('\n');
+		_putchar('$');
 	else
 	{
 		while (i < n)
@@ -19,8 +19,8 @@ void print_line(int n)
 			_putchar('_');
 			i++;
 		}
+		_putchar('$');
 	}
-	_putchar('$');
 	_putchar('\n');
 }
 

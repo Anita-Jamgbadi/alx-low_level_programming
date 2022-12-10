ad7d34712016910d0d16c0ef4bf2402812fbf379
anita-jamgbadi <jamgbanita@gmail.com>
2022-12-10 01:06:31 +0000

    tasks

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp, *head;
 	unsigned int count;
 
+	tmp = NULL;
 	if (idx == 0)
 		tmp = add_dnodeint(h, n);
 	else

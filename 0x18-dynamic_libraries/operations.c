897d46dd69755e873b2a8b27da87f8011be762fe
anita-jamgbadi <jamgbanita@gmail.com>
2022-12-12 09:52:41 +0000

    all tasks

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - adds input
+ * @a:input
+ * @b: input
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts input
+ * @a: input
+ * @b: input
+ * Return - difference
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies input
+ * @a: input
+ * @b: input
+ * Return: Multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides input
+ * @a: input
+ * @b: input
+ * Return: division
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns the remainder of division of a and b
+ * @a: input
+ * @b: input
+ * Return: mod
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
